809d3318d607be9f9d5e1a6eefde0af355afa280
Ken 🏈 <me163@mac.com>
2025-10-15 09:28:28 -0700

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,14 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://moonkraken.github.io/shore",
   integrations: [
     starlight({
       title: "My Docs",
       social: [{
         icon: "github",
         label: "GitHub",
-        href: "https://github.com/MoonKraken/shore",
+        href: "https://moonkraken.github.io/shore",
       }],
       sidebar: [
         {
